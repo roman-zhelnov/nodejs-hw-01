9bd122d9a5b3ad5131c5a417b3beec0ab111edde
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-11-09 17:45:59 +0200

    add add-one contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const contacts = await readContacts();
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 addOneContact();
