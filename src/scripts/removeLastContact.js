1f9bd9fd7adbba57fb24a4ebaf2c12478e8f2e19
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-11-09 19:23:58 +0200

    add remove-last

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length > 0) {
+      contacts.pop();
+      await writeContacts(contacts);
+    }
+    return;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 removeLastContact();
